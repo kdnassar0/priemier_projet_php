89e142d56109a861832b84709eec419489b08687
kenan nassar <kdnassar0@gmail.com>
2022-10-06 11:19:31 +0200

    projet

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ajout produit</title>
+    <title>Ajout produits</title>
 </head>
 <body>
     <h1>Ajouter un produit</h1>
