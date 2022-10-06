5516036c2c408fda2099c81a64780372b6447b27
kenan nassar <kdnassar0@gmail.com>
2022-10-06 11:18:09 +0200

    projet

---
+++ traitement.php
@@ -0,0 +1,5 @@
+<!--
+    Traitera la requête provenant de la page index.php (après soumission du 
+    formulaire), ajoutera le produit avec son nom, son prix, sa quantité et le total calculé 
+    (prix × quantité) en session. 
+-->
