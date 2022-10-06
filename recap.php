91c6c12cf42f5dbfca9e3a3a55172cae9e641677
kenan nassar <kdnassar0@gmail.com>
2022-10-06 15:05:00 +0200

    ajouter recap , ajout en session de produit

--- recap.php
+++ recap.php
@@ -2,3 +2,24 @@
     Affichera tous les produits en session (et en détail) et présentera le total général de 
     tous les produits ajoutés 
 -->
+
+
+<?php 
+session_start() ; 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Récapitulatif des produits</title>
+</head>
+<body>
+    <?php var_dump($_SESSION)?>
+    
+</body>
+</html>
+
+
--- traitement.php
+++ traitement.php
@@ -6,10 +6,26 @@
 <?php
 
 session_start() ; 
-if(isset($_POST['submit'])){
+if(isset($_POST['submit']))
+{
+    $name = filter_input(INPUT_POST,"name",FILTER_SANITIZE_STRING)  ;
+    $price = filter_input(INPUT_POST,"price",FILTER_VALIDATE_FLOAT,FILTER_FLAG_ALLOW_FRACTION);  ;
+    $qtt = filter_input(INPUT_POST,"qtt",FILTER_VALIDATE_INT)  ;
 
-    echo "bonjour";
+    if($name && $price && $qtt)
+
+    {
+      $product = ["name" => $name , 
+                 "price" => $price , 
+                "qtt" => $qtt,
+                "total" =>$price * $qtt 
+            ];
+      $_SESSION['products'][] =$product ;      
+   }
+
 }
-//header("location:index.php") ; 
+
+header("LOCATION :index.php") ;
+
 
 ?>
